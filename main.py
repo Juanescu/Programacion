a8f857334a6fba84d92b5ad74fcce836bbaaea41
Juanescu <141734682+Juanescu@users.noreply.github.com>
2023-08-23 23:30:08 -0300

    Ejercicios Complementarios

---
+++ main.py
@@ -0,0 +1,67 @@
+#1. Asignación de variable "numero1"
+numero1= 17
+print(numero1)
+#2. Asignación de variable "numero2"  
+numero2= 2.5
+print(numero2)
+#3. Asignación de variable "suma" que almacena las variables anteruires
+suma= numero1+numero2
+print(suma)
+#4. Asignación de 3 variables
+resta= numero1-numero2
+multi= numero1*numero2
+div= numero1/numero2
+print(resta)
+print(multi)
+print(div)
+#5. Asignación de variable "nombre"
+nombre= "Juan"
+print(nombre)
+#6. Asignación de variable "precio"
+precio= 280.5
+print(precio)
+#7. Asignacion de variable "descuento"
+descuento= 0.50
+print(descuento)
+#8. Asignacion y calculo de la variable "precio_final"
+precio_final= precio-(precio*descuento)
+print(precio_final)
+#9. Asignacion de la variable "cadena"
+cadena= "Buenos días"
+print(cadena)
+#10. Asignacion de la variable "longitud"
+longitud= len(cadena)
+print(longitud)
+#11. Asignacion de la variable "precio" en decimal y conversion a entero
+precio= 50.7
+precio= int(precio)
+print(precio)
+#12. Creacion y contcatenacion de la variables "nombre" y "apellido"
+nombre= "Juan"
+apellido= "Escudero"
+nombre_completo= "Juan" +" "+ "Escudero"
+print(nombre_completo)
+#13. Asignacion y calculo de la variable "edad" 
+edad= 19
+print(edad)
+edad= edad+5
+print(edad)
+edad= edad-10
+print(edad)
+#14. Asignacion y calculo de la variable "altura"
+altura= 1.68
+print(altura)
+altura= altura*4
+print(altura)
+altura= altura/3
+print(altura)
+#15. Transfornación de un nombre a minusculas
+nombre_mayus= "JUAN CRUZ"
+print(nombre_mayus)
+nombre_minus= nombre_mayus.lower()
+print(nombre_minus)
+#16. Transformacion de la variable "nombre_mayus"
+nombre_min= nombre_mayus.title()
+print(nombre_min)
+
+
